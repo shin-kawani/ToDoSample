1183ddbdc1c3242a8821f94103d69b83e5c97db8
shin.kawani <shin.kawani@RJ-MN-267.local>
2015-03-03 20:34:20 +0900

    Initial Commit

---
+++ ToDoSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoSample
+//
+//  Created by shin.kawani on 3/3/15.
+//  Copyright (c) 2015 shin.kawani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDoSample
+//
+//  Created by shin.kawani on 3/3/15.
+//  Copyright (c) 2015 shin.kawani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
